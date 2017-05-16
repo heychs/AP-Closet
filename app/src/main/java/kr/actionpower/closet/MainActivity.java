a50b2922df4d39d7da2586c5efe2df3a99cd94a3
Hwansuk Choi <heychs@gmail.com>
2017-05-16 10:51:38 +0900

    AI Closet Android Application Initial Commit

---
+++ app/src/main/java/kr/actionpower/closet/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.actionpower.closet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
